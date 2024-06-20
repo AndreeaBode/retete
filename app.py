d0f607b289419b186a5f86c61686840362f7c14d
Andreea Bode <92944991+AndreeaBode@users.noreply.github.com>
2024-06-20 21:04:17 +0300

    f

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 from flask import Flask, request, jsonify
 import joblib
+import os
 
 app = Flask(__name__)
 
 # Încarcă modelul
-model = joblib.load('mlb.pkl')
+model_path = 'mlb.pkl'
+if os.path.exists(model_path):
+    model = joblib.load(model_path)
+else:
+    raise FileNotFoundError(f"Model file {model_path} not found.")
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
         data = request.json  # se presupune că datele sunt trimise ca JSON
-        
+        # Verifică dacă datele sunt un dict sau o listă
+        if not isinstance(data, (list, dict)):
+            raise ValueError("Input data should be a list or a dictionary.")
+
         # Realizează predicția
         prediction = model.predict(data)
         
