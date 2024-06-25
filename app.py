106fa7263a6dd9324dec8c13868f99e7d1ac3bda
Andreea Bode <92944991+AndreeaBode@users.noreply.github.com>
2024-06-25 21:02:24 +0300

    final commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/predict": {"origins": "https://licenta-and-3c665baef0ba.herokuapp.com"}})
 
 # Load the model
 model_path = 'mlb.pkl'
