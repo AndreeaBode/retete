b1fb06f922a7e36143bd94fdc5191d73904c43fb
Andreea Bode <92944991+AndreeaBode@users.noreply.github.com>
2024-06-26 18:27:44 +0300

    final commit

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ classifier_path = 'classifier_model.pkl'
 mlb_path = 'mlb.pkl'
 vectorizer_path = 'vectorizer.pkl'
 
+
 if os.path.exists(classifier_path) and os.path.exists(mlb_path) and os.path.exists(vectorizer_path):
     classifier = joblib.load(classifier_path)
     mlb = joblib.load(mlb_path)
@@ -25,25 +26,46 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
+        # Log the receipt of a request
+        print("Received a prediction request.")
+        
         # Assume data is sent as JSON
         data = request.json
+        
+        # Log the received data
+        print(f"Received data: {data}")
         
         # Check if data is a list
         if not isinstance(data, list):
             raise ValueError("Input data should be a list of ingredients.")
-
+        
+        # Log before transforming data
+        print(f"Transforming data: {data}")
+        
         # Transform the input data to the format expected by the classifier
         input_vector = vectorizer.transform([" ".join(data)])
+        
+        # Log before making a prediction
+        print("Making prediction.")
         
         # Make the prediction
         prediction = classifier.predict(input_vector)
         
+        # Log the prediction result
+        print(f"Prediction result: {prediction}")
+        
         # Inverse transform the prediction to get the original labels
         predicted_labels = mlb.inverse_transform(prediction)
+        
+        # Log the predicted labels
+        print(f"Predicted Food Type Labels: {predicted_labels}")
         
         # Return the prediction result
         return jsonify({'prediction': list(predicted_labels)}), 200
     except Exception as e:
+        # Log the error
+        print(f"Error occurred: {e}")
+        
         # In case of an error, return the error message
         return jsonify({'error': str(e)}), 400
 
